5dad4d16921c1a2af371f313f23d5ead35a808a6
unknown <shenjie@snail.com>
2021-11-26 17:43:50 +0800

    Simulate server and client  communication.

--- SilverBulletEngine/SilverBulletEngine/SilverBulletEngine.cpp
+++ SilverBulletEngine/SilverBulletEngine/SilverBulletEngine.cpp
@@ -1,20 +1,147 @@
 ﻿// SilverBulletEngine.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
+#undef UNICODE
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
 #include <iostream>
+#include <string>
+#include <fstream>
+
+#pragma comment (lib, "Ws2_32.lib")
+#define DEFAULT_BUFLEN 256
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::cout << "****************\n*    SERVER    *\n****************\n\n";
+	
+
+	char str[INET_ADDRSTRLEN];
+
+	// Initialize Winsock
+	WSADATA wsaData;
+	int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != NO_ERROR) {
+		std::cout << "WSAStartup failed with error:" << iResult << std::endl;
+		return 1;
+	}
+
+	// Create a socket to listening the incoming connection requests
+	SOCKET ListenSocket, ClientSocket;
+	ListenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (ListenSocket == INVALID_SOCKET) {
+		std::cout << "Socket failed with error: " << WSAGetLastError() << std::endl;
+		WSACleanup();
+		return 1;
+	}
+
+
+	sockaddr_in addrServer;
+	addrServer.sin_family = AF_INET;
+	InetPton(AF_INET, "127.0.0.1", &addrServer.sin_addr.s_addr);
+	addrServer.sin_port = htons(6666);
+	memset(&(addrServer.sin_zero), '\0', 8);
+
+	// Bind socket
+	if (bind(ListenSocket, (SOCKADDR *)& addrServer, sizeof(addrServer)) == SOCKET_ERROR) {
+		std::cout << "Bind failed with error: " << WSAGetLastError() << std::endl;
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+	}
+	
+
+	// Listen for incoming connection
+	if (listen(ListenSocket, 5) == SOCKET_ERROR){
+		std::cout << "Listen failed with error: " << WSAGetLastError() << std::endl;
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	// Accept a client socket
+	ClientSocket = accept(ListenSocket, NULL, NULL);
+	if (ClientSocket == INVALID_SOCKET) {
+		std::cout << "Accept failed with error: " << WSAGetLastError() << std::endl;
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	// Variables for receive
+	int iSendResult;
+	char recvbuf[DEFAULT_BUFLEN];
+	int recvbuflen = DEFAULT_BUFLEN;
+
+	// Receive data
+	iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
+	if (iResult > 0) {
+		std::string filename;
+
+		for (int i = 0; i < iResult; i++){
+			filename += recvbuf[i];
+		}
+
+		//std::fstream file;
+		//file.open(filename, std::ios::in | std::ios::binary);
+
+		//file.seekg(0, std::ios::end);
+		//int fileSize = file.tellg();
+		//file.close();
+		std::cout << "File name: " << filename << std::endl;
+
+
+		std::string temp = filename;
+		char tempc[DEFAULT_BUFLEN];
+		int i = 0;
+		while (temp[i] != '\0')
+		{	
+			tempc[i] = temp[i];
+			i++;
+		}
+		tempc[i] = '\0';
+
+		const char* sendbuf = tempc;
+
+		// send file size to client
+		iSendResult = send(ClientSocket, sendbuf, (int)strlen(sendbuf), 0);
+		if (iSendResult == SOCKET_ERROR) {
+			std::cout << "Send failed with error: " << WSAGetLastError() << std::endl;
+			closesocket(ClientSocket);
+			WSACleanup();
+			return 1;
+		}
+	}
+	else if (iResult == 0) {
+		std::cout << "Connection closing... \n" << std::endl;
+	}
+	else {
+		std::cout << "Recieve failed with error: " << WSAGetLastError() << std::endl;
+		closesocket(ClientSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	std::cout << "Connection closing...\n" << std::endl;
+
+	//Shutdown the connection since we're done
+	iResult = shutdown(ClientSocket, SD_SEND);
+	if (iResult == SOCKET_ERROR) {
+		printf("shutdown failed with error: %d\n", WSAGetLastError());
+		closesocket(ClientSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	// cleanup
+	closesocket(ClientSocket);
+	WSACleanup();
+
+	return 0;
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
---
+++ TestClientDemo/TestClientDemo/TestClientDemo.cpp
@@ -0,0 +1,113 @@
+﻿// TestClientDemo.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+
+
+#undef UNICODE
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+
+
+#pragma comment (lib, "Ws2_32.lib")
+#define DEFAULT_BUFLEN 256
+
+int main()
+{
+	std::cout << "****************\n*    CLIENT    *\n****************\n\n";
+
+	// Initialize Winsock
+	WSADATA wsaData;
+
+	int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != NO_ERROR) {
+		std::cout << "WSAStartup Failed with error: " << iResult << std::endl;
+		return 1;
+	}
+
+	SOCKET ConnectSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (ConnectSocket == INVALID_SOCKET) {
+		std::cout << "Error at socket(): " << WSAGetLastError() << std::endl;
+		WSACleanup();
+		return 1;
+	}
+
+	sockaddr_in addrServer;
+	addrServer.sin_family = AF_INET;
+	InetPton(AF_INET, "127.0.0.1", &addrServer.sin_addr.s_addr);
+	addrServer.sin_port = htons(6666);
+	memset(&(addrServer.sin_zero), '\0', 8);
+
+	std::cout << "Connecting..." << std::endl;
+	iResult = connect(ConnectSocket, (SOCKADDR *)&addrServer, sizeof(addrServer));
+	if (iResult == SOCKET_ERROR) {
+		closesocket(ConnectSocket);
+		std::cout << "Unable to connect to server: " << WSAGetLastError() << std::endl;
+		WSACleanup();
+		return 1;
+	}
+
+
+	char filename[DEFAULT_BUFLEN] = { 0 };
+	std::cout << "Name of file: ";
+	std::cin.getline(filename, DEFAULT_BUFLEN, '\n');
+
+	const char *sendbuf = filename;
+	char recvbuf[DEFAULT_BUFLEN];
+	int recvbuflen = DEFAULT_BUFLEN;
+
+	// Send file name
+	iResult = send(ConnectSocket, sendbuf, (int)strlen(sendbuf), 0);
+	if (iResult == SOCKET_ERROR) {
+		std::cout << "Send failed with error: " << WSAGetLastError() << std::endl;
+		WSACleanup();
+		return 1;
+	}
+
+	do {
+		iResult = recv(ConnectSocket, recvbuf, recvbuflen, 0);
+		if (iResult > 0) {
+			std::string size;
+			for (int i = 0; i < iResult; i++) size += recvbuf[i];
+
+			if (size == "-1") {
+				std::cout << "No file named \"" << filename << "\"" << std::endl;
+			}
+			else {
+				std::cout << "The size of file \"" << filename << "\" is: " << size << std::endl;
+			}
+		}
+		else if (iResult == 0) {
+			std::cout << "Connection closed\n" << std::endl;
+		}
+		else
+		{
+			printf("recv failed with error: %d\n", WSAGetLastError());
+		}
+	
+	} while (iResult > 0);
+
+
+	// shutdown the connection
+	iResult = shutdown(ConnectSocket, SD_SEND);
+	if (iResult == SOCKET_ERROR) {
+		printf("shutdown failed with error: %d\n", WSAGetLastError());
+		closesocket(ConnectSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	// cleanup
+	closesocket(ConnectSocket);
+	WSACleanup();
+
+	return 1;
+}
+
+
